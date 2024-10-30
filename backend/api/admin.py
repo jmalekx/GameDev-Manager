2ed5e26ae31ccf96209f8927b28c86859969a842
jmalekx <151661788+jmalekx@users.noreply.github.com>
2024-10-30 21:05:15 +0000

    Added 2 classes

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Developer
+from .models import Game
 
-# Register your models here.
+@admin.register(Developer)
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ('name', 'experience_years', 'available_to_hire')
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'platform', 'release_date')
+    list_filter = ('platform',)
+    search_fields = ('title',)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Developer(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.TextField()
+    experience_years = models.IntegerField()
+    available_to_hire = models.BooleanField(default=True)
+    join_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+
+    PC = 'PC'
+    Console = 'Console'
+    Mobile = 'Mobile'
+    PLATFORM_TYPE = [
+        (PC, 'Computer/Laptop'),
+        (Console, 'Console'),
+        (Mobile, 'Android/iOS'),
+    ]
+
+    platform = models.CharField(
+        max_length=7,
+        choices=PLATFORM_TYPE,
+        default=PC,
+    )
+
+    def __str__(self):
+        return f"{self.title} ({self.get_platform_display()})"
